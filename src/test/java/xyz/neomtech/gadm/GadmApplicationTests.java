ae4db68c079107c1236289d9436d094a4ff222e4
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-21 13:34:25 +0600

    init commit!

---
+++ src/test/java/xyz/neomtech/gadm/GadmApplicationTests.java
@@ -0,0 +1,13 @@
+//package xyz.neomtech.gadm;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GadmApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
