aa14757140098d5424d3e087edc05c03d14c99f5
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-21 14:34:55 +0600

    new code remove due to test code add!

---
+++ src/test/java/xyz/neomtech/gadm/bangladesh/GadmApplicationTests.java
@@ -0,0 +1,18 @@
+package xyz.neomtech.gadm.bangladesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void workingFine() {
+        System.out.println("everything working fine!");
+    }
+
+}
